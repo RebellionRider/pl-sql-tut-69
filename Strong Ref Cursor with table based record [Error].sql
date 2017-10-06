f2587013590d5982e86754440a3e3dbc9a233041
RebellionRider <32567588+RebellionRider@users.noreply.github.com>
2017-10-06 17:13:53 +0530

    Update Strong Ref Cursor with table based record [Error].sql

--- Strong Ref Cursor with table based record [Error].sql
+++ Strong Ref Cursor with table based record [Error].sql
@@ -1,15 +1,17 @@
 /*
 Strong Ref Cursor with Table based record datatype can not retrive data from single column. 
-It will give you an error 
+It will give you an error.
+On execution of this program you will get an "PLS-00382: expression is of wrong type" error
 */
 SET SERVEROUTPUT ON
 DECLARE
     rec_var     employees%ROWTYPE;
-    /*Declare a Ref Cursor/pointer Type*/
+    /*Declare a Ref Cursor/pointer Type using TABLE BASED RECORD datatype*/
     TYPE RefCur    IS REF CURSOR RETURN employees%ROWTYPE;
     /*Declare a Cursor Variable of above Ref Cursor Type*/
     cur_var RefCur;
 BEGIN
+    --Here we are opening our cursor for a select statement which is returining the data from one column only this will cause an error
     OPEN cur_var FOR SELECT salary FROM employees WHERE employee_id = 100;
     FETCH cur_var INTO rec_var;
     CLOSE cur_var;
